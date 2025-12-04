f947a233895dacba9b9c3fb30e5fb704571e012c
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-04 16:30:14 +0530

    added todays

--- src/form.jsx
+++ src/form.jsx
@@ -9,3 +9,17 @@ export const Form = () => {
         console.log(name,email)
     }
   return (
+    <>
+    <div>Form handling</div>
+    <form action="" onSubmit={Handlesubmit}>
+        enter name : <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}} />
+        enter email : <input type="text" value={email}  onChange={(e)=>{setemail(e.target.value)}} />
+        <button type='submit'>save data</button>
+    </form>
+    </>
+  )
+}
+
+export default Form
+
+// controlled component = jise react se controlled krte h
