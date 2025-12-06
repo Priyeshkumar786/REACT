79b0e452753bb2ae53204245a4638eaaa5cadd47
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-06 21:18:25 +0530

    form validation

--- src/Hook3(form).jsx
+++ src/Hook3(form).jsx
@@ -3,3 +3,12 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+
+const Hook3 = () => {
+
+  // let[name,setname]=useState("")
+  // let[email,setemail]=useState("")
+
+  let navigate = useNavigate()
+
+  
