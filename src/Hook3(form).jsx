87d24d65fbe8e3229a1097e815aeecee9739df94
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-06 21:19:45 +0530

    form validation

--- src/Hook3(form).jsx
+++ src/Hook3(form).jsx
@@ -30,3 +30,36 @@ setform({...form ,[e.target.name] : e.target.value})
     // console.log(name,email);
     e.preventDefault() ;
     console.log(form.name);
+
+    let valid=true
+    
+
+    if(form.name.trim()==""){
+      alert("name cannot be empty")
+      valid=false
+    }
+
+    else if(form.email.trim()==""){
+      alert("email cannot be empty")
+      valid=false
+    }
+
+    else if(form.number.trim()==""){
+      alert("number cannot be empty")
+      valid=false
+    }
+
+    if(valid){
+        alert("form submitted successfully")
+
+        localStorage.setItem("Name",form.name)
+        localStorage.setItem("Email",form.email)
+        localStorage.setItem("Number",form.number)
+    }
+    if(form.age.length<18 ){
+      alert("enter correct age")
+    }
+
+    
+  }
+ 
