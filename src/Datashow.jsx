ec236b21568e0b2e4dffcd1cbab2c951878f20f1
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-12 16:32:33 +0530

    added todays topic

--- src/Datashow.jsx
+++ src/Datashow.jsx
@@ -4,6 +4,25 @@ import React, { useEffect } from 'react'
 const Datashow = () => {
 
 
+    useEffect(()=>{
+
+
+        // let res = fetch(api , {method:"GET"})  js 
+
+        let api = "ttp://localhost:3000/hotel"
+        axios.get(api).then(  (res)=>{
+
+            console.log(res);
+            
+        }).catch((err)=>{
+            console.log("Error",err);
+            
+
+        })
+
+    },[])                                 
+
+
 
 
     
