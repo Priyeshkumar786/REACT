091e5776982b37fb3739acb7067468ffc68305f4
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-18 22:35:16 +0530

    added todays work

--- src/Hook.jsx
+++ src/Hook.jsx
@@ -128,3 +128,33 @@ import React, { useState } from 'react'
 
 
 // =============================================================================
+// const Hook = () => {
+//     let [name ,setname]=useState("")
+//     let [city ,setcity]=useState("")
+
+//     let inputchange=(e)=>{
+//         console.log(e.target.value);
+//         setname(e.target.value)
+        
+//     }
+//     <br />
+
+//     let inputchange1=(v)=>{
+//         console.log(v.target.value);
+//         setcity(v.target.value)
+        
+//     }
+//     return(
+//         <>
+        
+//         Enter name: <input type="text" onChange={inputchange} />
+//         <br />
+
+//         Your name is {name}
+
+//         <br />
+
+//          Enter city: <input type="text" onChange={inputchange1} />
+//         <br />
+
+//         Your City name is {city}
