3663adfef119c64847b021b5877409d682804a71
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-20 12:13:39 +0530

    added todays question of react

--- src/Datashow.jsx
+++ src/Datashow.jsx
@@ -241,6 +241,27 @@ function FruitList() {
 }
 
 export default FruitList;
+// ----------------------------------------------------------------------
+
+import React, { useState } from 'react';
+
+function InputExample() {
+  const [name, setName] = useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h3>Hello, {name}</h3>
+    </div>
+  );
+}
+
+export default InputExample;
 
 
 
