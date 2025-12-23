18fd1e78c84684c3a282c09b159de72fb213ac2e
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-23 16:20:17 +0530

    added todays topic

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -1,2 +1,3 @@
-propse drill it is used to get to inherit the parents data 
-//it always inherit parent value not a grandparent value
+// propse drill it is used to get to inherit the parents data 
+// it always inherit parent value not a grandparent value
+// iit basically always inherits parents data not grandparents data
