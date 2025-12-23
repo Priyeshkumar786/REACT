f099b2996416606616fb92782a1dab67d0e87b2b
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-23 16:34:55 +0530

    added todays topic

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -1,3 +1,5 @@
 // propse drill it is used to get to inherit the parents data 
 // it always inherit parent value not a grandparent value
 // iit basically always inherits parents data not grandparents data
+
+// Other functions are used in let UserContext = createContext()
