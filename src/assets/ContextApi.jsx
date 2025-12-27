cb64c5cd680abf578f5651b6d1facee121e078d6
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-27 23:25:34 +0530

    added new question

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -31,3 +31,26 @@
 //useeffects takes the syntax ----it controlm the sideeffect of any function
 // usememo used in every function and publically 
 // props is must 
+
+
+
+
+
+
+
+
+
+
+// -----------------------question------------------------
+
+import { forwardRef } from 'react';
+
+const MyInput = forwardRef(function MyInput(props, ref) {
+  const { label, ...otherProps } = props;
+  return (
+    <label>
+      {label}
+      <input {...otherProps} />
+    </label>
+  );
+});
