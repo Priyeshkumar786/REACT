9a513eb66d77f129a6812672733ac38213575e91
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-27 23:26:33 +0530

    added new question

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -54,3 +54,18 @@ const MyInput = forwardRef(function MyInput(props, ref) {
     </label>
   );
 });
+
+
+// ------------------------
+
+import { forwardRef } from 'react';
+
+const MyInput = forwardRef(function MyInput(props, ref) {
+  const { label, ...otherProps } = props;
+  return (
+    <label>
+      {label}
+      <input {...otherProps} ref={ref} />
+    </label>
+  );
+});
