36b8079b7ebc93bcd0252e5bd5439fdabae049fd
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-27 23:27:18 +0530

    added new question

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -69,3 +69,21 @@ const MyInput = forwardRef(function MyInput(props, ref) {
     </label>
   );
 });
+
+// ======================================
+function Form() {
+  const ref = useRef(null);
+
+  function handleClick() {
+    ref.current.focus();
+  }
+
+  return (
+    <form>
+      <MyInput label="Enter your name:" ref={ref} />
+      <button type="button" onClick={handleClick}>
+        Edit
+      </button>
+    </form>
+  );
+}
