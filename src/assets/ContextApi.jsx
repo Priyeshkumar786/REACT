8584d78bc9b8505e6e90e3b2531c771fb19872e7
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-28 22:46:27 +0530

    added todays theory questions

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -43,17 +43,17 @@
 
 // -----------------------question------------------------
 
-import { forwardRef } from 'react';
+// import { forwardRef } from 'react';
 
-const MyInput = forwardRef(function MyInput(props, ref) {
-  const { label, ...otherProps } = props;
-  return (
-    <label>
-      {label}
-      <input {...otherProps} />
-    </label>
-  );
-});
+// const MyInput = forwardRef(function MyInput(props, ref) {
+//   const { label, ...otherProps } = props;
+//   return (
+//     <label>
+//       {label}
+//       <input {...otherProps} />
+//     </label>
+//   );
+// });
 
 
 // ------------------------
