c8983ebbf1feb71214d126b3412a889c315fe40e
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-05 23:45:01 +0530

    added todays all

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -375,3 +375,20 @@ Managing focus, text selection, or media playback.
 Triggering imperative animations.
 
 Integrating with third-party DOM libraries.
+
+
+
+
+What are props in React?
+Entry
+Props are inputs to a React component. They are single values or objects containing a set of values that are passed to React Components on creation using a naming convention similar to HTML-tag attributes. i.e, They are data passed down from a parent component to a child component.
+
+The primary purpose of props in React is to provide the following component functionality:
+
+Pass custom data to your React component.
+
+Trigger state changes.
+
+Use via this.props.reactProp inside component's render() method.
+
+For example, let us create an element with reactProp property,
