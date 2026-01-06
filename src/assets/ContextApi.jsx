711963dbe2f5ae15366de6370eeddcdfbd50f1d8
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-06 22:51:09 +0530

    starting imp template

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -400,3 +400,7 @@ Entry
 Context provides a way to pass data through the component tree without having to pass props down manually at every level.
 
 Context is designed to share data that can be considered “global” for a tree of React components, such as the current authenticated user, theme, or preferred language. Using context, we can avoid passing props through intermediate elements.
+
+// -------------------
+
+import { createContext } from "react";
