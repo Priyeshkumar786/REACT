66dae495b08dcef02111ded9638ebdf08205f13b
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-06 22:52:02 +0530

    starting imp template

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -408,4 +408,5 @@ const UserContext = createContext();
 `export default UserContext;
 // -------------------
 
-// In the above code, we have created a context using createContext() method and exported it to be used in other components.`
+// In the above code, we have created a context using createContext() method and exported it to be used in other components.
+// To provide a context value to the component tree, we can use the Provider component that comes with the context object.`
