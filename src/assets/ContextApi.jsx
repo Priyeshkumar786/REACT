dddfa42822f8deb30b17529a5b74d5acf97a4db6
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-01-14 22:31:57 +0530

    aded todays new

--- src/assets/ContextApi.jsx
+++ src/assets/ContextApi.jsx
@@ -489,3 +489,20 @@ function ParentComponent() {
 
 // // Example usage:
 // dispatch({ type: 'increment' });
+
+import { useRef } from 'react';
+
+function App() {
+  const inputRef = useRef();
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" />
+      <button onClick={focusInput}>Focus</button>
+    </div>
+  );
+}
