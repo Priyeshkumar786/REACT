61d68c829817772f40c0695005d46c48848d925f
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2026-02-08 21:34:28 +0530

    added todays work

--- src/dummy.jsx
+++ src/dummy.jsx
@@ -48,3 +48,10 @@ const element = (
         {name}.Welcome to GeeksforGeeks.
     </h1>
 );
+const name = "Learner";
+const element = (
+    <h1>
+        Hello,
+        {name}.Welcome to GeeksforGeeks.
+    </h1>
+);
